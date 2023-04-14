2f2c3d42df97d4b85042c4e700ab55981450c908
Victor-Dev0 <vitinhomatascav@hotmail.com>
2023-04-14 15:01:08 -0300

    Projetos Finalizados

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+        {
+            numero: '38111',
+            nome: 'Fulano de Tal',
+            partido: 'ABC',
+            fotos: [
+                {url:'1.jpg', legenda: 'Vereador'}
+            ]
+        },
+        {
+            numero: '77222',
+            nome: 'Beltrano de Tal',
+            partido: 'DEFG',
+            fotos: [
+                {url: '1-2.jpg', legenda: 'Vereador'}
+            ]
+        },
+    ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url:'2.jpg', legenda:'Prefeito'},
+                    {url:'2-2.jpg',legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '88',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url:'3.jpg', legenda:'Prefeito'},
+                    {url:'3-2.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
